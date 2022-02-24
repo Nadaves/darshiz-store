013f5f638d90aa7f475674a4a624d44bf65a89e4
Nadav <61387301+Nadaves@users.noreply.github.com>
2022-02-24 23:03:14 +0200

    localhost

--- src/components/requestMethods.js
+++ src/components/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://159.223.28.5:5000/api";
+const BASE_URL = "http://localhost:5000/api";
 const TOKEN = JSON.parse;
 JSON.parse(localStorage.getItem("persist:root"));
 
